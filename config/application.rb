ea0034ba5ef440da01ffea77ebd6388ca300e126
Alexander Shvets <alexander.shvets@gmail.com>
2013-06-27 10:01:24 -0400

    upgrade to rails 4

--- config/application.rb
+++ config/application.rb
@@ -4,33 +4,15 @@ require 'rails/all'
 #require 'shadow_db_credentials/shadow_db_credentials'
 require 'zip_dsl'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-
-  groups = {
-    :current => %W(default #{Rails.env})
-  }
-
-  Bundler.require(*Rails.groups(*groups[:current]))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
 
 module RailsDemo
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/view_classes)
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -40,22 +22,8 @@ module RailsDemo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
-    # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    # Custom directories with classes and modules you want to be autoloadable.
+    config.autoload_paths += %W(#{config.root}/view_classes)
 
     def config.database_configuration
       result = super
@@ -78,6 +46,5 @@ module RailsDemo
 
       result
     end
-
   end
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsDemo::Application.config.secret_token = 'd38b43ef6a84a7673f5d4a1de6a1fc4bb5bb2d8e1de2f10ec6603af2521963235fa5e3f41bf05413baf1dad3b72bf34f3dff6496072184c03cf0b9d904076c99'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsDemo::Application.config.secret_key_base = '456dcbe1a3c2457bc72db39bc3741b40d5580ab7a7e9e31f4df8122710d13d2ceeaf55acbcacb57cf033a2a7e445da1cb4bd1aaa12b81eafc6cda7c4ed504ff7'
